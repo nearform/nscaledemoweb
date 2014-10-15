def405d1777a81f60123ae94184bfd754fefda39
Peter Elger <elger.peter@gmail.com>
2014-10-15 15:09:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var http = require('http');
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
+}).listen(8000);
+
+console.log('Server started');
+
