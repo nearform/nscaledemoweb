2f9f7ddadc8bead84de4a74665085d362b1854ff
Matteo Collina <hello@matteocollina.com>
2015-02-20 13:04:37 +0000

    Added process.env.PORT support.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 var http = require('http');
+var port = process.env.PORT || 8000;
 
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
-}).listen(8000);
+}).listen(port, function() {
+  console.log('Server started on port', port);
+});
 
-console.log('Server started');
 
